10f4b65326551e0ee30875097c7de6f5f3a1b1e8
Flower <Z3072533636@163.com>
2022-11-24 11:17:59 +0800

    "equipment about html and view"

--- Video_app/dashboard/urls.py
+++ Video_app/dashboard/urls.py
@@ -1,6 +1,7 @@
 # encoding:utf-8
 from django.urls import path
+from views.base import Base
 
 urlpatterns = [
-
+    path('base/', Base.as_view(), name='base')
 ]
---
+++ Video_app/dashboard/views/base.py
@@ -0,0 +1,14 @@
+# encoding:utf-8
+
+from django.views.generic import View
+from Video_app.lib import base_render
+from django.shortcuts import render
+
+
+class Base(View):
+
+    TEMPLATE = 'dashboard/Base.html'
+
+    def get(self, request):
+        return render(request, self.TEMPLATE)
+
